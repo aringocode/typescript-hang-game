62e829f400f58282d98279dc0ee00a026646547f
akonshina <arina.konshina@teskalabs.com>
2024-12-02 15:36:14 +0100

    add body parts and styles

--- src/HangmanPicture.tsx
+++ src/HangmanPicture.tsx
@@ -1,9 +1,39 @@
 import './HangmanPicture.css';
 
+const HEAD = (
+	<div className='hangman-head' />
+);
+
+const BODY = (
+	<div className='hangman-body' />
+);
+
+const RIGHT_ARM = (
+	<div className='hangman-right-arm' />
+);
+
+const LEFT_ARM = (
+	<div className='hangman-left-arm' />
+);
+
+const RIGHT_LEG = (
+	<div className='hangman-right-leg' />
+);
+
+const LEFT_LEG = (
+	<div className='hangman-left-leg' />
+);
+
 const HangmanPicture = () => {
 
 	return (
 		<div className='hangman-picture-wrapper'>
+			{HEAD}
+			{BODY}
+			{RIGHT_ARM}
+			{LEFT_ARM}
+			{RIGHT_LEG}
+			{LEFT_LEG}
 			<div className='hangman-gallows-vertical' />
 			<div className='hangman-gallows' />
 			<div className='hangman-vertical' />
