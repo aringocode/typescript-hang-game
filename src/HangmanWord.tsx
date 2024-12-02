e1e48894ca6b38882b695aa5bae95741c1b1c3ea
akonshina <arina.konshina@teskalabs.com>
2024-12-02 15:46:05 +0100

    implement HangmanWord

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,5 +1,23 @@
-const HangmanWord = () => {
+import './HangmanWord.css';
 
+const HangmanWord = () => {
+	const word = 'test'
+	const guessedLetters = ['t', 'e', 'g']
+	return (
+		<div className='hangman-word-wrapper'>
+			{word.split('').map((letter, idx) => (
+				<span key={idx} className='hangman-letter'>
+					<span
+						style={{
+							visibility: guessedLetters.includes(letter) ? 'visible' : 'hidden',
+						}}
+					>
+						{letter}
+					</span>
+				</span>
+			))}
+		</div>
+	);
 }
 
 export default HangmanWord;
